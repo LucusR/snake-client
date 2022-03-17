8d05ebf9b9c3493bb9c54ef6b4f14dec968ef426
Lucus Riccioni <lucusroni@gmail.com>
2022-03-17 01:31:36 +0000

    Snake Game

--- play.js
+++ play.js
@@ -1,32 +1,28 @@
-const net = require("net");
+const connect = require("./client")
 
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "165.227.47.243",
-    port: 50541,
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  conn.on("data", (data) => {
-    console.log(data);
-  });
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  return conn;
+const handleUserInput = function (key) {
+  if (key === '\u0003') {
+    process.exit();
+  }
 };
 
-console.log("Connecting ...");
+
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput)
+  return stdin;
+};
+
+
+
+
+setupInput();
 connect();
+
+
+console.log("Connecting ...");
+
