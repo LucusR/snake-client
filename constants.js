2bafeb46226ca7bee31e03fd056b0e8366c744b0
Lucus Riccioni <lucusroni@gmail.com>
2022-03-17 02:50:22 +0000

    Snake Game Constants File

---
+++ constants.js
@@ -0,0 +1,23 @@
+const IP = "165.227.47.243"
+const PORT = 50541
+
+const UP = "Move: up"
+const DOWN = "Move: down"
+const LEFT = "Move: left"
+const RIGHT = "Move: right"
+
+const MSG1 = "Say: Hello"
+const MSG2 = "Say: Sorry"
+const MSG3 = "Say: Goodbye"
+
+module.exports = {
+IP,
+PORT,
+UP,
+DOWN,
+LEFT,
+RIGHT,
+MSG1,
+MSG2,
+MSG3,
+};
