ae50c084570be1be3a03003707bd592b1c21058e
Lucus Riccioni <lucusroni@gmail.com>
2022-03-17 02:49:51 +0000

    Snake Game Input File

--- input.js
+++ input.js
@@ -1,3 +1,5 @@
+const {UP, DOWN, LEFT, RIGHT, MSG1, MSG2, MSG3} = require("./constants");
+
 let connection;
 
 const setupInput = function(conn) {
@@ -15,16 +17,25 @@ const handleUserInput = function(key) {
     process.exit();
   }
   if (key === "w") {
-    connection.write("Move: up")
+    connection.write(UP)
   }
   if (key === "a") {
-    connection.write("Move: left")
+    connection.write(LEFT)
   }
   if (key === "s") {
-    connection.write("Move: down")
+    connection.write(DOWN)
   }
   if (key === "d") {
-    connection.write("Move: right")
+    connection.write(RIGHT)
+  }
+  if (key === "t") {
+    connection.write(MSG1)
+  }
+  if (key === "r") {
+    connection.write(MSG2)
+  }
+  if (key === "b") {
+    connection.write(MSG3)
   }
 };
 
